bb8e81d25fdf685b60c6b617bdc5de4716b5bb3a
jejimenezc <je.jimenezcastillo@gmail.com>
2025-08-22 00:08:30 -0400

    archivos clave actualizados para dejar todo en typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-// src/App.jsx
+// src/App.tsx
 import React from 'react';
+import type { JSX } from 'react'
 import { BlockEditorScreen } from './screens/BlockEditorScreen';
-
 import './App.css';
 
-function App() {
+export default function App(): JSX.Element {
   return (
     <div className="app-container">
       {/* Encabezado principal */}
-      <header className="app-header">
-        <h1>Mallas Curriculares</h1>
-      </header>
+      <header className="app-header"><h1>Mallas Curriculares</h1></header>
 
       {/* Contenido principal */}
-      <main className="app-main">
-        <BlockEditorScreen />
-      </main>
+      <main className="app-main"><BlockEditorScreen /></main>
     </div>
   );
 }
-
-export default App;
